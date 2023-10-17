3cb5faf93ccb25f7febad0e2abedfb26dfe8a548
madhurimapatra <112453293+madhurimapatra@users.noreply.github.com>
2023-10-17 11:30:23 +0530

    countdigits.cpp

---
+++ basic maths/countdigits.cpp
@@ -0,0 +1,11 @@
+int countDigits(int n){
+	 int temp=n, ans=0;
+    while(n>0){
+        int lastDigit= n%10;
+        n/=10;
+        if(lastDigit>0 && temp%lastDigit==0){
+            ans++;
+        }
+    }
+    return ans;	
+}
